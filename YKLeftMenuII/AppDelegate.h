61d330f1451ebed62200c2664adfa17a30806700
wangmchn <wangmchn@163.com>
2015-03-28 19:42:06 +0800

    Initial Commit

---
+++ YKLeftMenuII/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YKLeftMenuII
+//
+//  Created by Mark on 15/3/28.
+//  Copyright (c) 2015年 yq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YKLeftMenuII/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YKLeftMenuII
+//
+//  Created by Mark on 15/3/28.
+//  Copyright (c) 2015年 yq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
